afbd9180d5fb05dd0f0cc35117fa04a8d93ca486
Salivan Veerasekaran <salivan.v@scioms.local>
2025-10-23 20:03:19 +0530

    corectd

--- installer.sh
+++ installer.sh
@@ -1,3 +1,27 @@
 source .env
 
-echo "Starting installation of $APP_NAME version $APP_VERSION"
+
+
+# need to chek that docker is installed
+if ! [ -x "$(command -v docker)" ]; then
+    echo 'Error: docker is not installed.' >&2
+    exit 1
+fi
+
+# need to chek that docker-compose is installed
+if ! [ -x "$(command -v docker-compose)" ]; then
+    echo 'Error: docker-compose is not installed.' >&2
+    exit 1
+fi
+# need to chek that git is installed
+if ! [ -x "$(command -v git)" ]; then
+    echo 'Error: git is not installed.' >&2
+    exit 1
+fi
+# need to chek that curl is installed
+if ! [ -x "$(command -v curl)" ]; then
+    echo 'Error: curl is not installed.' >&2
+    exit 1
+fi
+# need to chek that unzip is installed
+if ! [ -x "$(command -v unzip)" ]; then
